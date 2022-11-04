03200b874b9b5e2fc317f7af9935e775f6bfff2b
Ayan Choudhary <ayanchoudhary1025@gmail.com>
2022-11-04 18:59:07 +0530

    feat: add source code for assignment

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://drive.google.com/file/d/16hoZEclhRUcGV4OwJ3kUGFPiQlL89c8U/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Download Assignment 1
+        </a>
+        <a
+          className="App-link"
+          href="https://drive.google.com/file/d/1Zhore5N7Yv2xhy8Q-tbomOH2uhhhksPR/view?usp=share_link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Download Assignment 2
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
